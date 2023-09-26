5c5cc64960e10c79fe4550000addfcb3e90f25bc
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-09-26 08:21:33 +0100

    Redesign idea page, add image rendering, add idea meta data cards.

--- pages/ideas/[id].tsx
+++ pages/ideas/[id].tsx
@@ -6,7 +6,10 @@ import { useShortAddress } from "@/utils/addressAndENSDisplayUtils";
 import moment from "moment";
 import { marked } from "marked";
 import DOMPurify from "isomorphic-dompurify";
-import { getIdea } from "@/graphql/types/__generated__/getIdea";
+import {
+  getIdea,
+  getIdea_getIdea,
+} from "@/graphql/types/__generated__/getIdea";
 import { getIdeaComments } from "@/graphql/types/__generated__/getIdeaComments";
 import { useLazyQuery, ApolloQueryResult } from "@apollo/client";
 import { GET_IDEA_QUERY } from "@/graphql/queries/ideaQuery";
@@ -22,6 +25,7 @@ import prisma from "@/lib/prisma";
 import { Community } from "@prisma/client";
 import { SUPPORTED_SUBDOMAINS } from "@/utils/supportedTokenUtils";
 import { client } from "@/lib/apollo";
+import { getTimeToClose } from "@/graphql/utils/queryUtils";
 
 const renderer = new marked.Renderer();
 const linkRenderer = renderer.link;
@@ -65,6 +69,128 @@ const ProfileLink = ({ id }: { id: string }) => {
   );
 };
 
+export const IdeaCard = ({ idea }: { idea: getIdea_getIdea }) => {
+  const { address } = useAccount();
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const { daysLeft, hoursLeft } = getTimeToClose(idea);
+
+  const buttonClass = isClient
+    ? address
+      ? "!bg-white !text-light-green"
+      : "!bg-grey/20 !text-grey !cursor-default"
+    : "";
+
+  return (
+    <div className="flex flex-col items-center gap-md self-stretch p-md rounded-xl border border-grey bg-white">
+      <div className="flex justify-between gap-md self-stretch">
+        <div className="text-black text-lg font-bold">Idea status</div>
+        <svg
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.9999 12.0004L11.9999 16.8004M11.9999 8.44258V8.40039M2.3999 12.0004C2.3999 6.69846 6.69797 2.40039 11.9999 2.40039C17.3018 2.40039 21.5999 6.69846 21.5999 12.0004C21.5999 17.3023 17.3018 21.6004 11.9999 21.6004C6.69797 21.6004 2.3999 17.3023 2.3999 12.0004Z"
+            stroke="black"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
+      <div className="flex flex-col items-start gap-2 self-stretch">
+        <div className="self-stretch text-black font-inter text-4xl font-bold leading-[44px]">
+          {idea.closed ? "Closed" : "Open"}
+        </div>
+        <div className="self-stretch text-slate font-inter text-sm font-semibold">
+          {daysLeft > 0
+            ? `Closes in ${daysLeft} day${daysLeft > 1 ? "s" : ""}`
+            : `Closes in ${hoursLeft} hour${hoursLeft > 1 ? "s" : ""}`}
+        </div>
+      </div>
+      <div className="flex flex-col items-start self-stretch">
+        <button
+          className={`${buttonClass} !border-grey border-[1px] !text-sm !rounded-[10px] !font-inter !font-bold !pt-sm !pb-sm !pl-md !pr-md self-center w-full`}
+          onClick={() => {
+            // Take to candidate prop page
+          }}
+        >
+          Submit a proposal
+        </button>
+      </div>
+      {isClient && !address && (
+        <div className="self-stretch text-slate font-inter text-sm">
+          Connect wallet to propose
+        </div>
+      )}
+
+      <div className="flex items-center gap-sm self-stretch">
+        <div className="flex flex-col justify-center items-start">
+          <div className="text-light-green text-sm font-semibold">
+            {/* TODO replace with proposal count on the idea. */}X proposals
+          </div>
+          <div className="self-stretch text-slate text-sm">
+            have been submitted
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const VotingCard = ({ idea, tokenBalance }: { idea: getIdea_getIdea; tokenBalance: number; }) => {
+  const { address } = useAccount();
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center gap-md self-stretch p-md rounded-xl border border-grey bg-white">
+      <div className="self-stretch text-black text-lg font-bold">
+        Voter sentiment
+      </div>
+      <div className="flex items-center gap-md self-stretch justify-between">
+        <IdeaVoteControls idea={idea} nounBalance={tokenBalance} />
+      </div>
+      <div className="flex items-center gap-md self-stretch">
+        <div className="nouns_dao_votes text-slate text-sm font-semibold">
+          Nouns DAO votes
+        </div>
+      </div>
+      <div className="flex flex-col items-start gap-md self-stretch p-2 rounded-xl bg-grey/40">
+        <div className="flex justify-between self-stretch">
+          <div className="text-black text-sm font-semibold">Upvotes</div>
+          <div className="flex text-slate text-sm">X</div>
+        </div>
+        <div className="flex justify-between self-stretch">
+          <div className="text-black text-sm font-semibold">Downvotes</div>
+          <div className="flex text-slate text-sm">X</div>
+        </div>
+        <div className="flex justify-between self-stretch">
+          <div className="text-black text-sm font-semibold">Consensus</div>
+          <div className="flex text-slate text-sm">{idea.consensus}%</div>
+        </div>
+      </div>
+      {isClient && !address && (
+        <div className="self-stretch text-slate text-sm">
+          Connect wallet to vote
+        </div>
+      )}
+    </div>
+  );
+};
+
 const IdeaPage = ({
   community,
   data,
@@ -76,16 +202,6 @@ const IdeaPage = ({
   const { id } = router.query as { id: string };
   const { address } = useAccount();
 
-  const [getIdeaCommentsQuery, { data: commentData }] =
-    useLazyQuery<getIdeaComments>(GET_IDEA_COMMENTS, {
-      context: {
-        clientName: "PropLot",
-        headers: {
-          "proplot-tz": Intl.DateTimeFormat().resolvedOptions().timeZone,
-        },
-      },
-    });
-
   const [getDelegatedVotes, { data: getDelegatedVotesData }] = useLazyQuery(
     DELEGATED_VOTES_BY_OWNER_SUB,
     {
@@ -94,14 +210,6 @@ const IdeaPage = ({
       },
     }
   );
-
-  useEffect(() => {
-    if (data.getIdea?.id) {
-      getIdeaCommentsQuery({
-        variables: { ideaId: data.getIdea?.id },
-      });
-    }
-  }, [getIdeaCommentsQuery, data.getIdea?.id]);
 
   useEffect(() => {
     if (address) {
@@ -124,24 +232,21 @@ const IdeaPage = ({
   const creatorTokenWeight = data.getIdea.votes?.find(
     (vote) => vote.voterId === data.getIdea?.creatorId
   )?.voterWeight;
-  const commentCount = commentData?.getIdeaComments?.filter(
-    (c: any) => !c.deleted
-  )?.length;
 
   return (
-    <Container fluid={"lg"} className="mt-xl mb-xl">
+    <Container fluid={"lg"} className="mt-xl mb-xl font-inter">
       <Row className="align-items-center">
-        <Col lg={10} className="mx-auto">
+        <Col lg={20} className="mx-auto">
           <Row>
             <Link
-              className="cursor-pointer text-dark-grey flex flex-row items-center"
+              className="cursor-pointer text-dark-grey flex flex-row mb-sm items-center"
               href="/"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="w-[24px] h-[24px] mr-sm cursor-pointer"
+                className="w-[18px] h-[18px] mr-xs cursor-pointer"
               >
                 <path
                   fillRule="evenodd"
@@ -150,7 +255,7 @@ const IdeaPage = ({
                 />
               </svg>
 
-              <span className="text-lg lodrina">Back</span>
+              <span className="text-sm">Back</span>
             </Link>
           </Row>
 
@@ -166,101 +271,62 @@ const IdeaPage = ({
             </div>
           )}
 
-          <Row>
-            <div className="flex flex-col mb-lg">
-              <div className="flex flex-row justify-between items-center">
-                <h1 className="mb-0 lodrina text-xxl self-end">
-                  {data.getIdea.title}
-                </h1>
-                <div className="flex flex-row justify-end">
-                  <IdeaVoteControls
-                    idea={data.getIdea}
-                    nounBalance={tokenBalance}
-                    withAvatars
+          <Row className="mt-md flex gap-lg">
+            <Col>
+              <Row>
+                <div className="flex flex-col mb-md">
+                  <div className="flex flex-row gap-sm flex-wrap text-sm text-dark-grey items-center">
+                    {data.getIdea.creatorId && (
+                      <ProfileLink id={data.getIdea.creatorId} />
+                    )}
+                    {` | ${moment(data.getIdea.createdAt).format(
+                      "MMM Do YYYY"
+                    )}`}
+                    {data.getIdea?.tags?.map((tag) => {
+                      return (
+                        <span
+                          key={tag.type}
+                          className={`${
+                            virtualTagColorMap[tag.type]?.colors ||
+                            "text-black bg-grey"
+                          } text-xs font-bold rounded-[6px] px-sm py-xs flex`}
+                        >
+                          {tag.label}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              </Row>
+              <div className="flex flex-col gap-lg">
+                <div className="flex flex-col gap-md">
+                  <h1 className="text-[36px] font-bold">
+                    {data.getIdea.title}
+                  </h1>
+                  <p className="font-semibold text-base">{data.getIdea.tldr}</p>
+                </div>
+
+                <div className="flex flex-col gap-md">
+                  <h3 className="font-bold text-lg">Details</h3>
+                  <div
+                    className="prose-base text-base text-slate"
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(
+                        marked.parse(data.getIdea.description),
+                        {
+                          ADD_ATTR: ["target"],
+                        }
+                      ),
+                    }}
                   />
                 </div>
               </div>
-              {data.getIdea.tags && data.getIdea.tags.length > 0 && (
-                <div className="flex flex-row gap-sm mt-lg flex-wrap">
-                  {data.getIdea.tags.map((tag) => {
-                    return (
-                      <span
-                        key={tag.label}
-                        className={`${
-                          virtualTagColorMap[tag.type]?.colors ||
-                          "text-white bg-blue"
-                        } text-xs font-bold rounded-[8px] px-sm py-xs flex`}
-                      >
-                        {tag.label}
-                      </span>
-                    );
-                  })}
-                </div>
-              )}
-            </div>
+            </Col>
+            <Col lg={4} className="md:max-w-[350px] flex flex-col gap-lg">
+              <IdeaCard idea={data.getIdea} />
+              <VotingCard idea={data.getIdea} tokenBalance={tokenBalance} />
+            </Col>
           </Row>
-          <div className="space-y-xl">
-            <div className="flex flex-col">
-              <h3 className="lodrina font-bold text-xl mb-sm">tl:dr</h3>
-              <p>{data.getIdea.tldr}</p>
-            </div>
-            <div className="flex flex-col">
-              <h3 className="lodrina font-bold text-xl mb-sm">Description</h3>
-              <div
-                className="prose-base"
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(
-                    marked.parse(data.getIdea.description),
-                    {
-                      ADD_ATTR: ["target"],
-                    }
-                  ),
-                }}
-              />
-            </div>
-          </div>
-
-          <div className="flex flex-1 font-bold text-sm text-dark-grey mt-xl whitespace-pre">
-            {data.getIdea.creatorId && (
-              <ProfileLink id={data.getIdea.creatorId} />
-            )}
-            {` | ${
-              creatorTokenWeight === 1
-                ? `${creatorTokenWeight} token`
-                : `${creatorTokenWeight} tokens`
-            } | ${moment(data.getIdea.createdAt).format("MMM Do YYYY")} ${
-              data.getIdea.closed ? "| closed" : ""
-            }`}
-          </div>
-
-          <div className="mt-sm mb-sm">
-            <h3 className="text-xl lodrina font-bold">
-              {commentCount} {commentCount === 1 ? "comment" : "comments"}
-            </h3>
-          </div>
-
-          <>
-            {!data.getIdea.closed && (
-              <CommentInput
-                hasTokens={hasTokens}
-                ideaId={id}
-                parentId={undefined}
-              />
-            )}
-            <div className="mt-xl space-y-xl">
-              {commentData?.getIdeaComments?.map((comment: any) => {
-                return (
-                  <Comment
-                    comment={comment}
-                    key={`comment-${comment.id}`}
-                    hasTokens={hasTokens}
-                    level={1}
-                    isIdeaClosed={!!data.getIdea?.closed}
-                  />
-                );
-              })}
-            </div>
-          </>
         </Col>
       </Row>
     </Container>
