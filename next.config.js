30ba646afc39bf092e96a969b29018696a422ff3
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-10-01 22:42:13 +0100

    Bump next.js config

--- next.config.js
+++ next.config.js
@@ -14,10 +14,18 @@ const nextConfig = {
     domains: ['i.seadn.io', 'proplot.twic.pics', 'proplot-images.s3.us-west-1.amazonaws.com'],
     loader: 'default',
   },
-  publicRuntimeConfig: {},
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_TWIC_PICS_DOMAIN: process.env.NEXT_PUBLIC_TWIC_PICS_DOMAIN,
+  },
   serverRuntimeConfig: {
     SECRET_AUTH_PASSWORD: process.env.SECRET_AUTH_PASSWORD,
     JSON_RPC_CLIENT: process.env.JSON_RPC_CLIENT,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_S3_DOMAIN: process.env.AWS_S3_DOMAIN,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    NEXT_PUBLIC_TWIC_PICS_DOMAIN: process.env.NEXT_PUBLIC_TWIC_PICS_DOMAIN,
   },
 }
 
