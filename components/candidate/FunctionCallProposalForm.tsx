8d5a878528ae98de3a126b53ed3a1a7efef26d5d
Michael Gingras <mcg79@cornell.edu>
2023-10-08 18:59:51 -0600

    shash

--- components/candidate/FunctionCallProposalForm.tsx
+++ components/candidate/FunctionCallProposalForm.tsx
@@ -87,7 +87,6 @@ const FunctionCallProposalForm = ({ register }: { register: any }) => {
       )}
       <div>
         <label>Import ABI</label>
-
         <input
           className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           id="file_input"
