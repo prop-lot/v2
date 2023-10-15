b14900690fab222220ba224a3b39b4b02dc050fd
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-10-15 11:10:14 +0100

    Remove Lil nouns support

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -14,22 +14,14 @@ const nounsDAOLink = new HttpLink({
   uri: 'https://api.goldsky.com/api/public/project_cldf2o9pqagp43svvbk5u3kmo/subgraphs/nouns/prod/gn',
 });
 
-const lilNounsDAOLink = new HttpLink({
-  uri: 'https://api.thegraph.com/subgraphs/name/lilnounsdao/lil-nouns-subgraph',
-});
-
 //pass them to apollo-client config
 export const client = new ApolloClient({
   ssrMode: typeof window === 'undefined',
   credentials: 'include',
   link: ApolloLink.split(
     operation => operation.getContext().clientName === SUPPORTED_SUBDOMAINS.NOUNS,
-    nounsDAOLink, //if above
-    ApolloLink.split(
-      operation => operation.getContext().clientName === SUPPORTED_SUBDOMAINS.LIL_NOUNS,
-      lilNounsDAOLink,
-      defaultLink,
-    ),
+    nounsDAOLink,
+    defaultLink,
   ),
   cache: new InMemoryCache(),
 });
--- utils/supportedTokenUtils.ts
+++ utils/supportedTokenUtils.ts
@@ -5,72 +5,10 @@ import { nounsGraphqlClient, lilNounsGraphqlClient } from '@/graphql/clients/nou
 
 // Extend this with subdomains for other DAOs that we want to integrate.
 export enum SUPPORTED_SUBDOMAINS {
-  LIL_NOUNS = 'lilnouns',
   NOUNS = 'nouns'
 }
 
 export const SupportedTokenGetterMap = {
-  [SUPPORTED_SUBDOMAINS.LIL_NOUNS]: {
-    account: "",
-    getUserTokenCount: async (address: string) => {
-      try {
-        const data: any = await lilNounsGraphqlClient.query(DELEGATED_VOTES_BY_OWNER_SUB, { id: address.toLowerCase() }).toPromise()
-        return parseInt(data?.data?.delegate?.delegatedVotes) || 0
-      } catch(e) {
-        throw new Error('Failed to fetch token count from subgraph')
-      }
-    },
-    getTotalSupply: async () => {
-      try {
-        const data: any = await lilNounsGraphqlClient.query(TOTAL_NOUNS_CREATED, {}).toPromise()
-        return parseInt(data?.data?.governance?.delegatedVotes) || undefined
-      } catch(e) {
-        throw new Error('Failed to fetch token supply from subgraph')
-      }
-    },
-    getUserVoteWeightAtBlock: async (address: string, blockNumber: number) => {
-      try {
-        const data: any = await lilNounsGraphqlClient.query(DELEGATED_VOTES_BY_OWNER_SUB_AT_BLOCK, { id: address.toLowerCase(), block: { number: blockNumber } }).toPromise()
-        return parseInt(data?.data?.delegate?.delegatedVotes) || 0
-      } catch(e) {
-        throw new Error('Failed to fetch token count from subgraph')
-      }
-    },
-    getCurrentAuction: async () => {
-      try {
-        const data: any = await lilNounsGraphqlClient.query(GET_CURRENT_AUCTION, {}).toPromise()
-        return data?.data?.auctions?.[0]
-      } catch(e) {
-        throw new Error('Failed to fetch current auction from subgraph')
-      }
-    },
-    getPreviousAuctions: async (nounIds: number[]) => {
-      try {
-        const data: any = await lilNounsGraphqlClient.query(GET_PREVIOUS_AUCTIONS, { nounIds }).toPromise()
-        return data?.data?.auctions
-      } catch(e) {
-        throw new Error('Failed to fetch current auction from subgraph')
-      }
-    },
-    getRecentProposals: async () => {
-      try {
-        const sixWeeksAgo = moment().subtract(6, 'weeks');
-        const recentTimestamp = sixWeeksAgo.unix();
-        const data: any = await lilNounsGraphqlClient.query(GET_RECENT_PROPOSALS, { recentTimestamp }).toPromise()
-        return data?.data?.proposals
-      } catch(e) {
-        throw new Error('Failed to fetch recent proposals from subgraph')
-      }
-    },
-    getGovernanceData: async () => {
-      try {
-        const data: any = await lilNounsGraphqlClient.query(GET_GOVERNANCE_DATA, { }).toPromise()
-        return data?.data?.governance
-      } catch(e) {
-        throw new Error('Failed to fetch governance data from subgraph')
-      }
-    },
-  },
   [SUPPORTED_SUBDOMAINS.NOUNS]: {
     account: "0x0bc3807ec262cb779b38d65b38158acc3bfede10",
     getUserTokenCount: async (address: string) => {
