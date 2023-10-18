946d75f3a241eb813145565033d1c57a43531f53
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-10-18 23:27:31 +0100

    Add CandidateRows to the idea page

--- pages/ideas/[id].tsx
+++ pages/ideas/[id].tsx
@@ -14,14 +14,19 @@ import { useLazyQuery, ApolloQueryResult } from "@apollo/client";
 import { GET_IDEA_QUERY } from "@/graphql/queries/ideaQuery";
 import { virtualTagColorMap } from "@/utils/virtualTagColors";
 import IdeaVoteControls from "@/components/IdeaVoteControls";
+import CandidateRow from "@/components/CandidateRow";
 import Link from "next/link";
-import { DELEGATED_VOTES_BY_OWNER_SUB } from "@/graphql/subgraph";
+import {
+  DELEGATED_VOTES_BY_OWNER_SUB,
+  GET_ALL_CANDIDATE_DATA,
+} from "@/graphql/subgraph";
 import { GetServerSidePropsContext } from "next";
 import prisma from "@/lib/prisma";
 import { Community } from "@prisma/client";
 import { SUPPORTED_SUBDOMAINS } from "@/utils/supportedTokenUtils";
 import { client } from "@/lib/apollo";
 import { getTimeToClose } from "@/graphql/utils/queryUtils";
+import { SupportedTokenGetterMap } from "../../utils/supportedTokenUtils";
 
 const renderer = new marked.Renderer();
 const linkRenderer = renderer.link;
@@ -132,10 +137,12 @@ export const IdeaCard = ({ idea }: { idea: getIdea_getIdea }) => {
       <div className="flex items-center gap-sm self-stretch">
         <div className="flex flex-col justify-center items-start">
           <div className="text-light-green text-sm font-semibold">
-            {/* TODO replace with proposal count on the idea. */}X proposals
+            {idea.candidates?.length === 1
+              ? `1 proposal`
+              : `${idea.candidates?.length} proposals`}
           </div>
           <div className="self-stretch text-slate text-sm">
-            have been submitted
+            {idea.candidates?.length === 1 ? `has` : `have`} been submitted
           </div>
         </div>
       </div>
@@ -214,6 +221,15 @@ const IdeaPage = ({
     }
   );
 
+  const [getAllCandidateData, { data: canadidatesData }] = useLazyQuery(
+    GET_ALL_CANDIDATE_DATA,
+    {
+      context: {
+        clientName: community?.uname as SUPPORTED_SUBDOMAINS,
+      },
+    }
+  );
+
   useEffect(() => {
     if (address) {
       getDelegatedVotes({
@@ -223,6 +239,18 @@ const IdeaPage = ({
       });
     }
   }, [address, getDelegatedVotes]);
+
+  useEffect(() => {
+    const slugs = data.getIdea?.candidates?.map((candidate) => candidate.slug);
+
+    if (slugs?.length === 1) {
+      getAllCandidateData({
+        variables: {
+          candidateSlugs: slugs,
+        },
+      });
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   // loading
   // todo: skeleton loading for better experience
@@ -237,102 +265,132 @@ const IdeaPage = ({
   )?.voterWeight;
 
   return (
-    <Container fluid={"lg"} className="mt-xl mb-xl font-inter">
-      <Row className="align-items-center">
-        <Col lg={20} className="mx-auto">
-          <Row>
-            <Link
-              className="cursor-pointer text-dark-grey flex flex-row mb-sm items-center"
-              href="/ideas"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-[18px] h-[18px] mr-xs cursor-pointer"
+    <div className="mt-xl">
+      <Row className="align-items-center bg-grey/30">
+        <Container fluid={"lg"} className="font-inter bg-white px-xl">
+          <Col lg={20} className="max-w-screen-xl mx-auto">
+            <Row>
+              <Link
+                className="cursor-pointer text-dark-grey flex flex-row mb-sm items-center"
+                href="/ideas"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z"
-                  clipRule="evenodd"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-[18px] h-[18px] mr-xs cursor-pointer"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+
+                <span className="text-sm">Back</span>
+              </Link>
+            </Row>
+
+            {data.getIdea.headerImage && (
+              <div className="flex flex-row w-full h-[320px] justify-center items-center flex-shrink-0 rounded-lg border border-[#A9B9CC] aspect-w-16 overflow-hidden">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  className="object-cover w-full h-full rounded-lg"
+                  alt="Header image"
+                  data-twic-src={`image:${data.getIdea.headerImage}`}
+                  src={`${process.env.NEXT_PUBLIC_TWIC_PICS_DOMAIN}/${data.getIdea.headerImage}?twic=v1/output=preview`}
                 />
-              </svg>
+              </div>
+            )}
 
-              <span className="text-sm">Back</span>
-            </Link>
-          </Row>
+            <Row className=" flex gap-lg">
+              <Col>
+                <Row>
+                  <div className="flex flex-col mb-md">
+                    <div className="flex flex-row gap-sm flex-wrap text-sm text-dark-grey items-center">
+                      {data.getIdea.creatorId && (
+                        <ProfileLink id={data.getIdea.creatorId} />
+                      )}
+                      {` | ${moment(data.getIdea.createdAt).format(
+                        "MMM Do YYYY"
+                      )}`}
+                      {data.getIdea?.tags?.map((tag) => {
+                        return (
+                          <span
+                            key={tag.type}
+                            className={`${
+                              virtualTagColorMap[tag.type]?.colors ||
+                              "text-black bg-grey"
+                            } text-xs font-bold rounded-[6px] px-sm py-xs flex`}
+                          >
+                            {tag.label}
+                          </span>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </Row>
+                <div className="flex flex-col gap-lg">
+                  <div className="flex flex-col gap-md">
+                    <h1 className="text-[36px] font-bold">
+                      {data.getIdea.title}
+                    </h1>
+                    <p className="font-semibold text-base">
+                      {data.getIdea.tldr}
+                    </p>
+                  </div>
 
-          {data.getIdea.headerImage && (
-            <div className="flex flex-row w-full h-[320px] justify-center items-center flex-shrink-0 rounded-lg border border-[#A9B9CC] aspect-w-16 overflow-hidden">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                className="object-cover w-full h-full rounded-lg"
-                alt="Header image"
-                data-twic-src={`image:${data.getIdea.headerImage}`}
-                src={`${process.env.NEXT_PUBLIC_TWIC_PICS_DOMAIN}/${data.getIdea.headerImage}?twic=v1/output=preview`}
-              />
-            </div>
-          )}
-
-          <Row className="mt-md flex gap-lg">
-            <Col>
-              <Row>
-                <div className="flex flex-col mb-md">
-                  <div className="flex flex-row gap-sm flex-wrap text-sm text-dark-grey items-center">
-                    {data.getIdea.creatorId && (
-                      <ProfileLink id={data.getIdea.creatorId} />
-                    )}
-                    {` | ${moment(data.getIdea.createdAt).format(
-                      "MMM Do YYYY"
-                    )}`}
-                    {data.getIdea?.tags?.map((tag) => {
-                      return (
-                        <span
-                          key={tag.type}
-                          className={`${
-                            virtualTagColorMap[tag.type]?.colors ||
-                            "text-black bg-grey"
-                          } text-xs font-bold rounded-[6px] px-sm py-xs flex`}
-                        >
-                          {tag.label}
-                        </span>
-                      );
-                    })}
+                  <div className="flex flex-col gap-md">
+                    <h3 className="font-bold text-lg">Details</h3>
+                    <div
+                      className="prose-base text-base text-slate"
+                      dangerouslySetInnerHTML={{
+                        __html: DOMPurify.sanitize(
+                          marked.parse(data.getIdea.description),
+                          {
+                            ADD_ATTR: ["target"],
+                          }
+                        ),
+                      }}
+                    />
                   </div>
                 </div>
-              </Row>
-              <div className="flex flex-col gap-lg">
-                <div className="flex flex-col gap-md">
-                  <h1 className="text-[36px] font-bold">
-                    {data.getIdea.title}
-                  </h1>
-                  <p className="font-semibold text-base">{data.getIdea.tldr}</p>
-                </div>
+              </Col>
+              <Col lg={4} className="md:max-w-[350px] flex flex-col gap-lg">
+                <IdeaCard idea={data.getIdea} />
+                <VotingCard idea={data.getIdea} tokenBalance={tokenBalance} />
+              </Col>
+            </Row>
+          </Col>
+        </Container>
 
-                <div className="flex flex-col gap-md">
-                  <h3 className="font-bold text-lg">Details</h3>
-                  <div
-                    className="prose-base text-base text-slate"
-                    dangerouslySetInnerHTML={{
-                      __html: DOMPurify.sanitize(
-                        marked.parse(data.getIdea.description),
-                        {
-                          ADD_ATTR: ["target"],
-                        }
-                      ),
-                    }}
-                  />
+        <Row className="flex gap-lg bg-white !px-0">
+          <div className="border-t border-b border-grey items-normal sm:items-center mt-lg">
+            <div className="max-w-screen-xl mx-auto w-full flex flex-row gap-lg p-md items-normal justify-between sm:items-center py-md px-xl">
+              <div className="flex flex-row gap-sm">
+                <div className="flex flex-col-reverse gap-lg sm:flex-row justify-between items-end sm:items-center">
+                  <div className="flex flex-1 flex-row items-center overflow-scroll gap-md">
+                    <button
+                      onClick={() => {}}
+                      key={"proposals"}
+                      className="flex gap-sm !bg-grey/80 !text-black !border-none !text-sm !rounded-[10px] !font-inter !pt-sm !pb-sm !pl-md !pr-md self-center"
+                    >
+                      Proposals
+                    </button>
+                  </div>
                 </div>
               </div>
-            </Col>
-            <Col lg={4} className="md:max-w-[350px] flex flex-col gap-lg">
-              <IdeaCard idea={data.getIdea} />
-              <VotingCard idea={data.getIdea} tokenBalance={tokenBalance} />
-            </Col>
-          </Row>
-        </Col>
+            </div>
+          </div>
+        </Row>
+
+        <Row className="max-w-screen-xl mx-auto py-xl px-xl flex gap-lg overflow-x-scroll">
+          {canadidatesData?.proposalCandidates?.map((candidate) => {
+            return <CandidateRow key={candidate.id} {...candidate} />;
+          })}
+        </Row>
       </Row>
-    </Container>
+    </div>
   );
 };
 
