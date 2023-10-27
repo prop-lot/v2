88c357ddc92dff0dd2d4ddc590fcb10e70d101b6
Tobias Hale <tobias.hale@deliveroo.co.uk>
2023-10-27 23:56:09 +0100

    remove fly.io related code, update config

--- next.config.js
+++ next.config.js
@@ -16,15 +16,16 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
     NEXT_PUBLIC_TWIC_PICS_DOMAIN: process.env.NEXT_PUBLIC_TWIC_PICS_DOMAIN,
+    NEXT_PUBLIC_JSON_RPC_CLIENT: process.env.NEXT_PUBLIC_JSON_RPC_CLIENT,
   },
   serverRuntimeConfig: {
     SECRET_AUTH_PASSWORD: process.env.SECRET_AUTH_PASSWORD,
-    JSON_RPC_CLIENT: process.env.JSON_RPC_CLIENT,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_REGION: process.env.AWS_REGION,
     AWS_S3_DOMAIN: process.env.AWS_S3_DOMAIN,
     S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    NEXT_PUBLIC_JSON_RPC_CLIENT: process.env.NEXT_PUBLIC_JSON_RPC_CLIENT,
     NEXT_PUBLIC_TWIC_PICS_DOMAIN: process.env.NEXT_PUBLIC_TWIC_PICS_DOMAIN,
   },
 }
